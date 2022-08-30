9793b42865e95e7425eb18335e95c508e0f924db
danzobiss <danzobiss@gmail.com>
2022-08-30 17:03:01 -0300

    SideBar

---
+++ src/components/SideBar/index.tsx
@@ -0,0 +1,47 @@
+import React from 'react';
+
+import List from '../List';
+import FollowSugestion from '../FollowSugestion';
+import News from '../News';
+import { Container, SearchWrapper, SearchInput, SearchIcon, Body } from './styles';
+
+const SideBar: React.FC = () => {
+    return(
+        <Container>
+            <SearchWrapper>
+                <SearchInput placeholder="Buscar no Twitter" />
+                <SearchIcon />
+            </SearchWrapper>
+
+            <Body>
+                <List 
+                    title='Talvez você curta'
+                    elements={[
+                        <FollowSugestion 
+                            name='Orihime Inoue'
+                            nickname='@inoue.orihime'
+                        />,
+                        <FollowSugestion 
+                            name='Chad'
+                            nickname='@sado.yasutora'
+                        />,
+                        <FollowSugestion 
+                            name='Último Quincy'
+                            nickname='@ishida.uryu'
+                        />
+                    ]}
+                />
+                <List 
+                    title='Talvez você curta'
+                    elements={[
+                        <News />,
+                        <News />,
+                        <News />
+                    ]}
+                />
+            </Body>
+        </Container>
+    );
+}
+
+export default SideBar;
