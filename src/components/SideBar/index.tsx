114d775715df107c966c646776898b95572fd8c3
danzobiss <danzobiss@gmail.com>
2022-08-30 17:05:50 -0300

    Fix scroll

--- src/components/SideBar/index.tsx
+++ src/components/SideBar/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import StickyBox from 'react-sticky-box';
 
 import List from '../List';
 import FollowSugestion from '../FollowSugestion';
@@ -13,33 +14,35 @@ const SideBar: React.FC = () => {
                 <SearchIcon />
             </SearchWrapper>
 
-            <Body>
-                <List 
-                    title='Talvez você curta'
-                    elements={[
-                        <FollowSugestion 
-                            name='Orihime Inoue'
-                            nickname='@inoue.orihime'
-                        />,
-                        <FollowSugestion 
-                            name='Chad'
-                            nickname='@sado.yasutora'
-                        />,
-                        <FollowSugestion 
-                            name='Último Quincy'
-                            nickname='@ishida.uryu'
-                        />
-                    ]}
-                />
-                <List 
-                    title='Talvez você curta'
-                    elements={[
-                        <News />,
-                        <News />,
-                        <News />
-                    ]}
-                />
-            </Body>
+            <StickyBox>
+                <Body>
+                    <List 
+                        title='Talvez você curta'
+                        elements={[
+                            <FollowSugestion 
+                                name='Orihime Inoue'
+                                nickname='@inoue.orihime'
+                            />,
+                            <FollowSugestion 
+                                name='Chad'
+                                nickname='@sado.yasutora'
+                            />,
+                            <FollowSugestion 
+                                name='Último Quincy'
+                                nickname='@ishida.uryu'
+                            />
+                        ]}
+                    />
+                    <List 
+                        title='Talvez você curta'
+                        elements={[
+                            <News />,
+                            <News />,
+                            <News />
+                        ]}
+                    />
+                </Body>
+            </StickyBox>
         </Container>
     );
 }
